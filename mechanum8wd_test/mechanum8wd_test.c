4b490558e5ab7bd542f91e4f6fe3c85d46601872
physalily <physalily@gmail.com>
2018-02-28 13:53:09 +0900

    [create]

---
+++ mechanum8wd_test/mechanum8wd_test.c
@@ -0,0 +1,31 @@
+/************************
+8wd mechanum test program
+________________________
+*************************/
+#include "mechanum.h"
+mechanum wheel_in(600,450,30);
+mechanum wheel_out(300,450,30);
+
+int main()
+{
+  double X, Y, A;
+  pinMode(,OUTPUT);
+  pinMode(,OUTPUT);
+  pinMode(,OUTPUT);
+  pinMode(,OUTPUT);
+
+  pinMode(,OUTPUT);
+  pinMode(,OUTPUT);
+  pinMode(,OUTPUT);
+  pinMode(,OUTPUT);
+
+  X = 500;
+  Y = 0;
+  A = 0;
+  wheel_in.update();
+  whee_out.update();
+  analogWrite()
+  while (TRUE) {
+
+  }
+}
