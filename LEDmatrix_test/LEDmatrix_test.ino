3977ec687126821c081eac99e33847b0d6d509b5
physalily <physalily@gmail.com>
2018-03-04 00:01:04 +0900

    [add]test code

--- LEDmatrix_test/LEDmatrix_test.ino
+++ LEDmatrix_test/LEDmatrix_test.ino
@@ -1,9 +1,15 @@
 int i;
 int dats[3][16] = {
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, //common
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //columnA
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, //columnB
 };
+
+int dats2[3][16] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //common
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, //columnA
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //columnB
+}
 
 int main()
 {
@@ -25,8 +31,20 @@ int main()
       digitalWrite(9,HIGH);//clock up
       digitalWrite(9,LOW);
     }
+    digitalWrite(7,HIGH);
+    digitalWrite(7,LOW);
+    //no delay time
+    for ( i = 0; i < 16; i++) {
+      //data set
+      digitalWrite(4,dats[0][i]);       //common
+      digitalWrite(5,dats[1][i]);       //columnA
+      digitalWrite(6,dats[2][i]);       //columnB
+      digitalWrite(9,HIGH);//clock up
+      digitalWrite(9,LOW);
+    }
     digitalWrite(7,HIGH);//latch
     digitalWrite(7,LOW);
+
     delay(1000);
   }
 }
